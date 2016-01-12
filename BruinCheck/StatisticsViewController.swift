b968e8be8a68a0cedda33ee75d6d166f420c5671
Matthew Allen Lin <Darthpwner@Matthews-MacBook-Pro.local>
2016-01-11 21:51:08 -0800

    Temporarily created a new View Controller

---
+++ BruinCheck/StatisticsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  StatisticsViewController.swift
+//  BruinCheck
+//
+//  Created by Matthew Allen Lin on 1/11/16.
+//  Copyright © 2016 Li-Wei Tseng. All rights reserved.
+//
+
+import Foundation
+
+class StatisticsViewController: UIViewController {
+    
+}
