b88ccd87dab2a45ee2e43bbdc3fb1137d51b3cc6
Matthew Allen Lin <Darthpwner@s-169-232-70-70.resnet.ucla.edu>
2016-01-17 20:08:19 -0800

    Have the basic set up for the TabBar

---
+++ BruinCheck/DashboardTabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  StatisticsViewController.swift
+//  BruinCheck
+//
+//  Created by Matthew Allen Lin on 1/11/16.
+//  Copyright © 2016 Li-Wei Tseng. All rights reserved.
+//
+
+import Foundation
+
+class DashboardTabBarController: UITabBarController, UITabBarControllerDelegate {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        delegate = self
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        let item1 = ViewController()
+        let icon1 = UITabBarItem(title: "Title", image: UIImage(named: "someImage.png"), selectedImage: UIImage(named: "otherImage.png"))
+        item1.tabBarItem = icon1
+        let controllers = [item1]  //array of the root view controllers displayed by the tab bar interface
+        self.viewControllers = controllers
+    }
+    
+    //Delegate methods
+    func tabBarController(tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool {
+        print("Should select viewController: \(viewController.title) ?")
+        return true;
+    }
+}
